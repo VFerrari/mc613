7305ae973672890bdada945f1e1c6e604704c0b0
Victor Ferrao Santolim <ra187888@barbuda.lab.ic.unicamp.br>
2018-05-21 18:16:08 -0300

    Inicializador adicionado entrada semi-funcional

--- trab/boliche.vhd
+++ trab/boliche.vhd
@@ -1,5 +1,7 @@
 library ieee ; 
-use ieee.std_logic_1164.all ; 
+use ieee.std_logic_1164.all ;
+
+use work.boliche_pack.all;
 
 entity boliche is
 port (SW  : in std_logic_vector(9 downto 0) ; 
@@ -24,9 +26,9 @@ signal n_jog : std_logic_vector(2 downto 0);
 signal disp_ini : std_logic_vector(6 downto 0);
 
 begin
-	maq : controle port map(CLOCK_50, not(KEY(0)), avanca, secao);
+	maq : controle port map(CLOCK_50, not(KEY(0)), fim_ini, secao);
 	
-	--ini: inicializador port map(CLOCK_50, secao(0), not(KEY(3)), not(KEY(0)), SW(6 downto 1), fim_ini, n_jog, disp_ini);
+	ini: inicializador port map(CLOCK_50, secao(0), not(KEY(3)), SW(6 downto 1), fim_ini, n_jog, disp_ini);
 	--meio: andamento port map(CLOCK_50, secao(1), not (KEY(0)), SW);
 	--fim: final port map(CLOCK_50, secao(2), not(KEY(0)), );
 	
---
+++ trab/inicializador.vhd
@@ -0,0 +1,58 @@
+library ieee ; 
+use ieee.std_logic_1164.all ; 
+
+entity inicializador is
+port (CLOCK_50 : in std_logic;
+		enable   : in std_logic;
+		confirma : in std_logic;
+		quant    : in std_logic_vector(5 downto 0);
+		fim		: out std_logic;
+		n_jog		: out std_logic_vector(2 downto 0);
+		display  : out std_logic_vector(6 downto 0)
+		);
+end inicializador ; 
+
+architecture logica of inicializador is
+
+signal n_jog_select : std_logic_vector(2 downto 0);
+
+begin
+
+	with quant select
+		n_jog_select <= "001" when "000001",
+							 "010" when "000010",
+							 "011" when "000100",
+							 "100" when "001000",
+							 "101" when "010000",
+							 "110" when "100000",
+							 "000" when others;
+							 
+	with quant select
+		display 		 <= "1111001" when "000001",
+							 "0100100" when "000010",
+							 "0110000" when "000100",
+							 "0011001" when "001000",
+							 "0010010" when "010000",
+							 "0000010" when "100000",
+							 "0000110" when others;
+							 
+							
+	process(CLOCK_50, enable, confirma, n_jog_select)
+	begin
+		if (rising_edge(CLOCK_50)) then
+			if (enable = '1' and confirma = '1' and n_jog_select /= "000") then
+			
+				n_jog <= n_jog_select;
+				fim <= '1';
+			
+			else
+				n_jog <= "000";
+				fim <= '0';
+				
+			end if;
+		end if;
+	end process;
+	
+	
+
+end logica;
